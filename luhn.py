0ffbec2d5955bea101d610a4af79fedff83e90f3
kgoodman33 <kgoodman33@gmail.com>
2015-01-12 11:08:14 -0800

    Luhn Algorithm

--- luhn.py
+++ luhn.py
@@ -10,11 +10,11 @@ nums = []
 
 for i in number:
 	nums.append(i)
-
+last_digit = nums.pop()
 nums2 = nums[::-1]
 
 for i in range(len(nums2)):
-	if i % 2 != 1:
+	if i % 2 == 1:
 		nums2[i] = int(nums2[i]) * 2
 	else:
 		nums2[i] = int(nums2[i])
@@ -38,6 +38,10 @@ nums3 = ''.join(nums)
 print "The check digit is: %d " % check_digit
 print "The card number is: %r" % int(nums3)
 
+if int(check_digit) == int(last_digit):
+	print "The card is valid!"
+else:
+	print "The car is invalid!"
 
 
 
