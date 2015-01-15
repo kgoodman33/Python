3f99f73c692b38ccad34f3f1df58385340f1bb39
kgoodman33 <kgoodman33@gmail.com>
2015-01-15 12:05:43 -0800

    Luhn Algorithm
    Checks the validity of credit card numbers

---
+++ luhnalgorithm2.py
@@ -0,0 +1,54 @@
+import sys
+
+
+if sys.argv[1:]:
+	number = sys.argv[1]
+else:
+	number = raw_input("What's the number, fool? > ")
+
+nums = []
+
+for i in number:
+	nums.append(i)
+last_digit = nums.pop()
+nums2 = nums[::-1]
+
+for i in range(len(nums2)):
+	if i % 2 == 0:
+		nums2[i] = int(nums2[i]) * 2
+	else:
+		nums2[i] = int(nums2[i])
+
+for i in range(len(nums2)):
+	if int((nums2[i]) >= 10):
+		z = ((int(nums2[i]) / 10) + (int(nums2[i]) % 10))
+		nums2[i] = int(z)
+
+sum = 0
+for i in range(len(nums2)):
+	sum += nums2[i]
+
+check_digit = (sum * 9) % 10
+
+
+nums.append(str(check_digit))
+nums3 = ''.join(nums)
+
+
+print "The check digit is: %d " % check_digit
+print "The card number is: %r" % int(nums3)
+
+if int(check_digit) == int(last_digit):
+	print "The card is valid!"
+else:
+	print "The car is invalid!"
+
+
+
+
+
+
+
+
+			
+		
